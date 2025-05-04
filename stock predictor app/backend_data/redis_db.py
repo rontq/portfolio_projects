e55e8e3da30ef14467ffe6469a330a7473277fc1
rontq <76719282+rontq@users.noreply.github.com>
2025-05-03 23:32:13 -0700

    dockered a redis db for real time updates

---
+++ stock predictor app/backend_data/redis_db.py
@@ -0,0 +1,9 @@
+import redis
+r = redis.Redis(host='localhost', port=6379, db=0)
+
+# Set a key
+r.set('mykey', 'Hello Worldfrom Redis!')
+
+# Get the key
+value = r.get('mykey')
+print(value.decode())  # Output: Hello from Redis!
